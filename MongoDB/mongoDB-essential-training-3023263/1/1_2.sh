0928beb9baad986d261e4530e846e03ae346ec88
LAPTOP-C8G0FBMK\kevin <zhibing.x@gmail.com>
2025-10-26 19:47:49 -0400

    update

---
+++ MongoDB/mongoDB-essential-training-3023263/1/1_2.sh
@@ -0,0 +1,9 @@
+npm version
+npm install -g m
+m latest
+mongod --version
+brew tap mongodb/brew
+brew install mongosh
+mongosh --version
+brew install mongodb-database-tools
+mongoimport
---
+++ MongoDB/mongoDB-essential-training-3023263/1/1_3.sh
@@ -0,0 +1,5 @@
+npm version
+sudo npm install -g m
+m latest
+mongod --version
+sudo apt-get install mongodb-database-tools
