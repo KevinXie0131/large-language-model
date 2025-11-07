c67a976c4bc104bfe5181da664481ad6afdde6e5
KevinX <Xzb04720638k>
2025-11-07 18:49:41 -0500

    update

--- RadditMQ/springboot2-java8-rabbitmq/src/main/java/hello/controller/GreetingController.java
+++ RadditMQ/springboot2-java8-rabbitmq/src/main/java/hello/controller/GreetingController.java
@@ -6,15 +6,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
 
 @RestController
 public class GreetingController {
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-
 
     @RequestMapping("/")
     public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
@@ -34,12 +38,73 @@ public class GreetingController {
         rabbitTemplate.convertAndSend(EXCHANGE_DIRECT, ROUTING_KEY, "Hello Rabbit!SpringBoot!");
     }
 
+
     @RequestMapping("/mq1")
     public void test7() throws InterruptedException {
         while(true){
             rabbitTemplate.convertAndSend(EXCHANGE_DIRECT, ROUTING_KEY, "Hello " + counter.incrementAndGet());
             Thread.sleep(100);
         }
-
     }
+
+
+    public static final String EXCHANGE_NORMAL = "exchange.normal.video";
+    public static final String EXCHANGE_DEAD_LETTER = "exchange.dead.letter.video";
+    public static final String ROUTING_KEY_NORMAL = "routing.key.normal.video";
+    public static final String ROUTING_KEY_DEAD_LETTER = "routing.key.dead.letter.video";
+    public static final String QUEUE_NORMAL = "queue.normal.video";
+    public static final String QUEUE_DEAD_LETTER = "queue.dead.letter.video";
+
+    @RequestMapping("/mq2")
+    public void test8() throws InterruptedException {
+        rabbitTemplate.convertAndSend(
+                        EXCHANGE_NORMAL,
+                        ROUTING_KEY_NORMAL,
+                        "测试死信情况1：消息被拒绝");
+    }
+
+    @RequestMapping("/mq3")
+    public void test9() throws InterruptedException {
+        for (int i = 0; i < 50; i++) {
+            rabbitTemplate.convertAndSend(
+                    EXCHANGE_NORMAL,
+                    ROUTING_KEY_NORMAL,
+                    "测试死信情况2：消息数量超过队列的最大容量" + i);
+        }
+    }
+
+
+    public static final String EXCHANGE_DELAY = "exchange.delay.video";
+    public static final String ROUTING_KEY_DELAY = "routing.key.delay.video";
+    public static final String QUEUE_DELAY = "queue.delay.video";
+
+    @RequestMapping("/mq4")
+    public void test10() throws InterruptedException {
+        rabbitTemplate.convertAndSend(
+                EXCHANGE_DELAY,
+                ROUTING_KEY_DELAY,
+                "测试基于插件的延迟消息 [" + new SimpleDateFormat("hh:mm:ss").format(new Date()) + "]",
+                messageProcessor -> {
+                    // 设置延迟时间：以毫秒为单位
+                    messageProcessor.getMessageProperties().setHeader("x-delay", "10000");
+
+                    return messageProcessor;
+                });
+    }
+
+    // 1、创建消息后置处理器对象
+    MessagePostProcessor messagePostProcessor = (Message message) -> {
+        // 设定 TTL 时间，以毫秒为单位
+        message.getMessageProperties().setHeader("x-delay", "5000");
+        return message;
+    };
+
+    @RequestMapping("/mq5")
+    public void test11() throws InterruptedException {
+        rabbitTemplate.convertAndSend(
+                EXCHANGE_DELAY,
+                ROUTING_KEY_DELAY,
+                "测试基于插件的延迟消息 [" + new SimpleDateFormat("hh:mm:ss").format(new Date()) + "]", messagePostProcessor);
+    }
+
 }
--- RadditMQ/springboot2-java8-rabbitmq/src/main/java/hello/listener/MyMessageConfig.java
+++ RadditMQ/springboot2-java8-rabbitmq/src/main/java/hello/listener/MyMessageConfig.java
@@ -18,13 +18,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
 @Configuration
 public class MyMessageConfig {
 
     public static final String EXCHANGE_DIRECT = "exchange.direct.order";
     public static final String ROUTING_KEY = "order";
     public static final String QUEUE_NAME = "queue.order";
-
 
     //    写法一：监听 + 在 RabbitMQ 服务器上创建交换机、队列
 /*    @RabbitListener(bindings = @QueueBinding(
@@ -36,8 +40,55 @@ public class MyMessageConfig {
 
     //    写法二：监听
     @RabbitListener(queues = {QUEUE_NAME})
-    public void processMessage(String dataString, Message message, Channel channel) {
-        System.out.println("消费端接收到了消息：" + dataString);
+    public void processMessage(String dataString, Message message, Channel channel) throws IOException, InterruptedException {
+        System.out.println("消费端1：" + dataString);
+
+        Thread.sleep((new Random()).nextInt(400));
+
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();
+        channel.basicAck(deliveryTag, false);
     }
 
+    @RabbitListener(queues = {QUEUE_NAME})
+    public void processMessage1(String dataString, Message message, Channel channel) throws IOException, InterruptedException {
+        System.out.println("消费端2：" + dataString);
+
+        Thread.sleep((new Random()).nextInt(400));
+
+        // 1、获取当前消息的 deliveryTag 值备用
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();
+        channel.basicAck(deliveryTag, false);
+    }
+
+    public static final String QUEUE_NORMAL = "queue.normal.video";
+    public static final String QUEUE_DEAD_LETTER = "queue.dead.letter.video";
+
+    @RabbitListener(queues = {QUEUE_NORMAL})
+    public void processMessageNormal(Message message, Channel channel) throws IOException, InterruptedException {
+        // 监听正常队列，但是拒绝消息
+        System.out.println("★[normal]消息接收到，但我拒绝。");
+        //  channel.basicReject(message.getMessageProperties().getDeliveryTag(), false);
+
+        channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, false);
+
+        //  channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+        //  Thread.sleep(100);
+    }
+
+    @RabbitListener(queues = {QUEUE_DEAD_LETTER})
+    public void processMessageDead(String dataString, Message message, Channel channel) throws IOException {
+        // 监听死信队列
+        System.out.println("★[dead letter]dataString = " + dataString);
+        System.out.println("★[dead letter]我是死信监听方法，我接收到了死信消息");
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+    }
+
+    public static final String QUEUE_DELAY = "queue.delay.video";
+
+    @RabbitListener(queues = {QUEUE_DELAY})
+    public void process(String dataString, Message message, Channel channel) throws IOException {
+        System.out.println("[生产者]" + dataString);
+        System.out.println("[消费者]" + new SimpleDateFormat("hh:mm:ss").format(new Date()));
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+    }
 }
